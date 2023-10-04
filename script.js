5ebcfea334c145c28b6b4f05a713fc6609403a25
Yash Shah <shahyash1902@gmail.com>
2023-10-04 17:45:07 -0300

    Fetched API of deck

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 Author: Yash Shah
 Date: 04/10/2023 
 */
+
+// fetching api's
+// 1. Deck API = https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+// 2. Cards API = https://deckofcardsapi.com/api/deck/1al773bcsnxg/draw/?count=5
+
+// Deck Api
+const deckAPI = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+let deckObject;
+
+fetch(deckAPI)
+      .then(response => response.json())
+      .then(json => {deckObject = json;
+    console.log(deckObject)})
+
+// Cards Api
