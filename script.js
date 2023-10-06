60b734baffdd0c27686301133e7bc9d4aa2fc33a
Yash Shah <shahyash1902@gmail.com>
2023-10-05 21:47:17 -0300

    New Commit 05/10/2023

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ Author: Yash Shah
 Date: 04/10/2023 
 */
 
+/**
+Transferred files from Own made Git repo: https://github.com/Yashshah1902/Assignment-2C-Prog1400.git
+*/
+
 /* DOM */
 
 // selecting body tag for background
 // document.body.style.backgroundImage = "url('./Poker-Hand-.png')";
-document.body.innerHTML = `<h1 id="heading">Assignment 2 - Poker Card</h1>`;
+document.body.innerHTML = `<marquee behavior="alternate"> <h1 id="heading">Assignment 2 - Highest Poker Hand</h1> </marquee>`;
 
 // Creating divs
-let mainDiv
+let mainDiv = document.createElement('div');
+mainDiv.setAttribute("id", "mainDiv");
+document.body.appendChild(mainDiv);
+
+/* Need to work one's finish Code */
+// let mainDivSelc = document.getElementById('mainDiv');
+// mainDivSelc.innerHTML += `<button id="start" onclick ="function()"> Draw cards! </button>`;
 
 let firstDiv = document.createElement('div'); // Creating div
 firstDiv.setAttribute("id", "imageDisplayer"); // Selected first image displayer for test, given id - #imageDisplayer
-document.body.appendChild(firstDiv); // Added first div
+mainDiv.appendChild(firstDiv); // Added first div
 
 
 // fetching api's
@@ -41,6 +51,7 @@ fetch(deckAPI)
         let cardsAPI = `https://deckofcardsapi.com/api/deck/${receivedDeckID}/draw/?count=5`;
 
         fetch(cardsAPI)
+        
         .then(response => response.json())
         .then(json => {cardObject = json
         console.log(cardObject);
@@ -49,9 +60,10 @@ fetch(deckAPI)
         imagesPngArray = allValues.imagesPng;
         suitsArray = allValues.suits;
         codesArray = allValues.codes;
-        });
 
         addingImages();
+        highestPokerHand();
+        });
         
     })
 
@@ -97,6 +109,80 @@ let addingImages = () => {
     let firstDivImage = document.getElementById('imageDisplayer');
     // firstDivImage.innerHTML = `<img src="${imagesPngArray[0]} alt="Card One">`;
     firstDivImage.innerHTML = "<h2>Images of the Shuffled Cards</h2>";
-    firstDivImage.innerHTML += `<image url=""`
 
+    // firstDivImage.innerHTML = `<img src="${imagesPngArray[0]}" alt="Card One"> <br> ${valuesArray[0]} of ${suitsArray[0]}`;
+
+    for (let x = 0; x < valuesArray.length; x++) {
+        firstDivImage.innerHTML += `<img src="${imagesPngArray[x]}" alt="Card One">`;
+    }
+
+    firstDivImage.innerHTML += `<ul>`;
+    for (let x = 0; x < valuesArray.length; x++) {
+        firstDivImage.innerHTML += `<li> <b>${valuesArray[x]} of ${suitsArray[x]}</b> </li>`;
+    }
+
+    firstDivImage.innerHTML += `</ul>`;
+
+};
+
+let highestPokerHand = () => {
+    // let suitSearch;
+    // let suitcheckVal = suitsArray[0];
+    // let checkingCount = 0;
+
+    // // "HEARTS" "CLUBS" "DIAMONDS" "SPADES"
+    // for (let index = 0; index < suitsArray.length; index++) {
+
+    //     if (suitsArray[index] === suitcheckVal) {
+    //         checkingCount++;
+    //     }  
+    // }
+
+    // if (checkingCount === suitsArray.length){
+    //     suitSearch = true;
+    // } else {
+    //     suitSearch = false;
+    // }
+
+
+    // if(suitSearch === true){
+
+    // } else {
+        
+    // }
+
+
+    // let result = console.log(suitSearch);
+    // console.log(checkingCount);
+    // console.log(suitsArray.length);
+    // console.log(valuesArray);
+
+    // return result;
+
+    let cloneValue = valuesArray.splice(0);
+    console.log(cloneValue);
+    // Replace, "ACE", "KING", "QUEEN", "JACK"  with ints
+    let replacedArray = cloneValue.map(element => {
+        switch (element) {
+            case "ACE":
+                return 14; // or 1, depending on your needs
+            case "KING":
+                return 13;
+            case "QUEEN":
+                return 12;
+            case "JACK":
+                return 11;
+            default:
+                // If it's not one of the specified values, leave it unchanged
+                return parseInt(element);
+        }
+    });
+
+    let highestInt = replacedArray[0];
+
+    
+    
+    console.log(replacedArray);
+    
+    return cloneValue;
 };
