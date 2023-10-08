5255d83ac2d0c36f7a462e5f20b32ef82594bded
Yash Shah <shahyash1902@gmail.com>
2023-10-08 01:11:39 -0300

    straight Flush condition Complete

--- script.js
+++ script.js
@@ -64,6 +64,7 @@ fetch(deckAPI)
 
         addingImages();
         console.log(royalFlush(cardObject));
+        console.log(straightFlush(cardObject));
         });
         
     })
@@ -204,17 +205,53 @@ let royalFlush = (object) => {
     for (let i = 0; i < (object.cards).length; i++) {
         testValue.push(object.cards[i].code.split(''));
     }
-    console.log(testValue);
+    // console.log(testValue);
 
     for (let i = 0; i < testValue.length; i++) {
         cards.push(testValue[i].shift());
         suits.push(testValue[i].pop());
     }
-    console.log(cards);
-    console.log(suits);
+    // console.log(cards);
+    // console.log(suits);
 
     return (
         suits.every(suit => suits[0] === suit) &&
         cards.every(card => royalCards.includes(card))
         );
     }
+
+let straightFlush = (object) => {
+    let suits = new Array;
+    let cards = new Array;
+    let testValue = new Array;
+
+    // This is only to test the outputs
+    // let cardCodes = cardObject.cards;
+    // console.log();
+
+    for (let i = 0; i < (object.cards).length; i++) {
+        testValue.push(object.cards[i].code.split(''));
+    }
+    // console.log(testValue);
+
+    for (let i = 0; i < testValue.length; i++) {
+        cards.push(testValue[i].shift());
+        suits.push(testValue[i].pop());
+    }
+    // console.log(cards);
+    // console.log(suits);
+
+    function areConsecutive(array) {
+        for (let i = 0; i < array.length - 1; i++) {
+          if (array[i] + 1 !== array[i + 1]) {
+            return false; // If a non-consecutive pair is found, return false
+          }
+        }
+        return true; // If all pairs are consecutive, return true
+    }
+
+    return (
+        suits.every(suit => suits[0] === suit) &&
+        areConsecutive(cards)
+    );
+}
