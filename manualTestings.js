b7cce60bb52707ad88acd676d664e47859f59142
Yash Shah <shahyash1902@gmail.com>
2023-10-08 22:37:16 -0300

    Code test complete fr straight Flush

--- manualTestings.js
+++ manualTestings.js
@@ -1,6 +1,7 @@
 // Manual Testing
 
 // Royal Flush
+let randomArray = ["2D", "4S", "JH", "QC", "KD"]
 let codesArray = ['AS', 'KS', 'QS', 'JS', '0S'];
 let royalFlush = (array) => {
 
@@ -46,50 +47,121 @@ let royalFlush = (array) => {
 
     // Straight Flush
 
-    let straightFlush = (array) => {
-        let suits = new Array;
-        let cards = new Array;
-        let testValue = new Array;
+    // let straightFlush = (array) => {
+    //     let suits = new Array;
+    //     let cards = new Array;
+    //     let testValue = new Array;
     
-        // This is only to test the outputs
-        // let cardCodes = cardObject.cards;
-        // console.log();
+    //     // This is only to test the outputs
+    //     // let cardCodes = cardObject.cards;
+    //     // console.log();
     
-        for (let i = 0; i < array.length; i++) {
-            let splitted = array[i].split('');
-            testValue.push(splitted);
-        }
-        // console.log(testValue);
+    //     for (let i = 0; i < array.length; i++) {
+    //         let splitted = array[i].split('');
+    //         testValue.push(splitted);
+    //     }
+    //     // console.log(testValue);
     
-        for (let i = 0; i < testValue.length; i++) {
-            cards.push(testValue[i].shift());
-            suits.push(testValue[i].pop());
-        }
+    //     for (let i = 0; i < testValue.length; i++) {
+    //         cards.push(testValue[i].shift());
+    //         suits.push(testValue[i].pop());
+    //     }
     
-        let replacedCards = cards.map((element) => {
-            if (element === "0"){
-                return "10";
+    //     let replacedCards = cards.map((element) => {
+    //         if (element === "0"){
+    //             return "10";
+    //         }
+    //         if(element === "A"){
+    //             return "14";
+    //         }
+    //         if (element === "K"){
+    //             return "13";
+    //         }
+    //         if (element === "Q"){
+    //             return "12";
+    //         }
+    //         if (element === "J"){
+    //             return "11";
+    //         }
+    
+    //         return element;
+    //     })
+        
+    //     console.log(replacedCards);
+    //     let replacedArray = replacedCards.map(element => parseInt(element));
+    //     console.log(replacedArray);
+    
+    //     function areConsecutive(array) {
+    //         for (let i = 0; i < array.length - 1; i++) {
+    //           if (array[i] + 1 !== array[i + 1]) {
+    //             return false; // If a non-consecutive pair is found, return false
+    //           }
+    //         }
+    //         return true; // If all pairs are consecutive, return true
+    //     }
+    
+    //     return (
+    //         suits.every(suit => suits[0] === suit) &&
+    //         areConsecutive(replacedArray)
+    //     );
+    // }
+
+    // Straight Flush repeat
+
+    let isStraightFlush = (array) => {
+        let suits = [];
+        let ranks = []
+
+        for (let card of array) {
+            let rank = card.slice(0, -1)
+            let suit = card.slice(-1);
+
+            if (rank === "0") {
+                rank = "10";
+            } else if (rank === "A") {
+                rank = "14";
+            } else if (rank === "K") {
+                rank = "13";
+            } else if (rank === "Q") {
+                rank = "12";
+            } else if (rank === "J") {
+                rank = "11";
             }
     
-            return element;
-        })
-    
-        function areConsecutive(array) {
-            for (let i = 0; i < array.length - 1; i++) {
-              if (array[i] + 1 !== array[i + 1]) {
-                return false; // If a non-consecutive pair is found, return false
-              }
+            ranks.push(parseInt(rank));
+            suits.push(suit);
+        }
+
+        console.log(suits);
+        console.log(ranks);
+
+        let sortedranks = ranks.sort();
+        console.log(sortedranks);
+
+        let count = 0;
+
+        for (let x = 0; x < suits.length; x++) {
+            if (suits[0] === suits[x]){
+                count++;
             }
-            return true; // If all pairs are consecutive, return true
+        }
+
+        if (count === 5){
+            for (let i = 0; i < sortedranks.length - 1; i++) {
+                if (sortedranks[i] + 1 !== sortedranks[i + 1]) {
+                    return false; // Not a straight flush
+                }
+            }
+            return true; // It's a straight flush
         }
     
-        return (
-            suits.every(suit => suits[0] === suit) &&
-            areConsecutive(replacedCards)
-        );
+        return false; // Not enough cards of the same suit for a flush
+
+        
     }
 
-    console.log(straightFlush(codesArray))
+    console.log(isStraightFlush(codesArray));
+    console.log(isStraightFlush(randomArray));
 
     // Four Of A Kind
 
